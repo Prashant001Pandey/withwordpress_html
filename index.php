6dc5b479d876dda235c94425ba4f938115d2ad91
Prashant Pandey <prashant001pandey@gmail.com>
2023-09-05 23:45:56 -0700

    adding new file

---
+++ index.php
@@ -0,0 +1,259 @@
+<?php // echo get_template_directory_uri(); 
+   // bloginfo('template_directory'); *
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regal Square, Indore - Corporate Co-working Space</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/style.css" />
+</head>
+<body>
+    <nav>
+        <div class="navbar">
+            <div class="navbar-left">
+                <div class="navbar-brand">
+                    <img src="<?php bloginfo('template_directory') ?>/Asset 2.svg" alt="Workviaa Logo">
+                </div>
+            </div>
+            <div class="navbar-right">
+                <ul>
+                    <li><a href="#home" class="sunflower-font"><h2>Home</h2></a></li>
+                    <li><a href="#locations" class="sunflower-font"><h2>Locations</h2></a></li>
+                    <li><a href="#products" class="sunflower-font"><h2>Products</h2></a></li>
+                    <li><span class="vertical-line2"></span></li> 
+                    <li><a href="#" class="contact-button sunflower-font"><h2>Contact</h2></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+    
+
+
+
+    <header class="header">
+        <div class="header-background">
+            <div class="header-content-box">
+                <div class="text-container">
+                    <div class="divider">
+                        <div class="divider"></div>
+                        <p><h1>Regal Square, Indore</h1></p>
+                        <div class="line"></div>
+                        <p><h2>Corporate House, 110, Block B, Indore,Madhya Pradesh 452001</h2></p>
+                    </div>
+                 </div>
+                <div>
+                    <p><h1>30,000 Sqft</h1></p>
+                </div>
+                <div class="vertical-line"></div>
+                    <p><h2>Corporate House Landmark</h2></p>
+                    <div class="vertical-line"></div>
+
+                    <div>
+                     <p><h2>Located at the heart of the city Regal Square</h2></p>
+                    </div>
+                    <a href="#" class="book-now-button"><h3>BOOK NOW</h3></a>
+                </div>
+            </div>
+            <a href="#" class="location-button monbaiti-font">Location &gt; Indore Corporate House</a>
+        </div>
+    </header>
+
+
+
+
+    <section id="space">
+        <div class="left-side">
+       
+            <ul>
+           
+                <h1 class="hr-lines">THE SPACE</h1> 
+                <li><h5>More than 30,000 square feet of co-working spaces and serviced offices</h5></li>
+                <li><h5>Spacious international standards meeting rooms</h5></li>
+                <li><h5>Large event space that can accommodate 100 persons</h5></li>
+                <li><h5>Full-service pantry and Cafeteria</h5></li>
+                <li><h5>Business grade Internet</h5></li>
+            </ul>
+        </div>
+        <div class="right-side">
+          
+                <img src="<?php bloginfo('template_directory') ?>/hd1.jpg" alt="Image Description">
+            
+        </div>
+    </section>
+    
+    <section id="premises">
+        <div class="left-side">
+            <ul>
+                <img src="<?php bloginfo('template_directory') ?>/hd2.jpg" alt="Image Description">
+            </ul>
+        </div>
+        <div class="left-side">
+           
+            <ul>
+                <h1 class="hr-lines">THE PREMISES</h1>
+                <li><h5>Conveniently located 2 minutes from Railway Station</h5></li>
+                <li><h5>Situated near Central Mall</h5></li>
+                <li><h5>Highly accessible bus stands 5 minutes away</h5></li>
+                <li><h5>Located at the heart of the city Regal Square</h5></li>
+                <li><h5>Heritage sites like Gandhi Hall Rajwada and High Court nearby</h5></li>
+            </ul>
+        </div>
+    </section>
+
+    <div class="centered-image">
+        <img src="<?php bloginfo('template_directory') ?>/img5.jpg" alt="Image Description">
+    </div>
+
+    <section class="amenities-section" style="background-color: #ef6b0d;">
+        <div class="section-title">OUR AMENITIES</div>
+        <div class="amenities-row">
+            <!-- First Row -->
+            <div class="amenity-card">
+                <img src="<?php bloginfo('template_directory') ?>/img/business_grade_internet.png" alt="Business Grade Internet">
+                <span class="amenity-name">Business Grade Internet</span>
+            </div>
+            <div class="amenity-card">
+                <img src="<?php bloginfo('template_directory') ?>/img/cafeteria.png" alt="Cafeteria">
+                <span class="amenity-name">Cafeteria</span>
+            </div>
+            <div class="amenity-card">
+                <img src="<?php bloginfo('template_directory') ?>/img/parking.png" alt="Parking">
+                <span class="amenity-name">Parking</span>
+            </div>
+            <div class="amenity-card">
+                <img src="<?php bloginfo('template_directory') ?>/img/meeting_room.png" alt="meeting room">
+                <span class="amenity-name">Meeting Room</span>
+            </div>
+        </div>
+        <div class="amenities-row">
+            <!-- Second Row -->
+            <div class="amenity-card">
+                <img src="<?php bloginfo('template_directory') ?>/img/power_backup.png" alt="Power Backup">
+                <span class="amenity-name">Power Backup</span>
+            </div>
+            <div class="amenity-card">
+                <img src="<?php bloginfo('template_directory') ?>/img/high_quality_printer.png" alt="High-Quality Printer">
+                <span class="amenity-name">High Quality Printer</span>
+            </div>
+            <div class="amenity-card">
+                <img src="<?php bloginfo('template_directory') ?>/img/Bestest_security.png" alt="Bestest Security">
+                <span class="amenity-name">Bestest Security</span>
+            </div>
+            <div class="amenity-card">
+                <img src="<?php bloginfo('template_directory') ?>/img/cleaning.png" alt="Cleaning">
+                <span class="amenity-name">Cleaning</span>
+            </div>
+        </div>
+        </section>
+    
+    
+    
+    <section id="availability">
+        <div style="text-align: center;">
+            <h1>Availability</h1>
+        </div>        
+    <div class="availability-container">
+        <div class="column">
+            <img src="<?php bloginfo('template_directory') ?>/so.jpg" alt="Standard Office Image">
+            <h3>Standard Office</h3> 
+            <ul> 
+            <li>Good for teams 1-24 pax</li>
+            <li>Move-in ready</li>
+            <li>Fully furnished private offices</li>
+            <li>Access to Shared Amenities</li>
+        </ul> 
+            <a href="#" class="book-now-button">BOOK NOW</a>
+        </div>
+
+        <div class="column">
+            <img src="<?php bloginfo('template_directory') ?>/dd.jpg" alt="Dedicated Desk Image">
+            <h3>Dedicated Desk</h3>
+            <ul>
+            <li>Good for individuals or small teams </li>
+            <li>Dedicated desks in a shared office</li>
+            <li>Access to meeting rooms, event spaces, and other Shared Amenities</li>
+            <li>Secured storage</li>
+        </ul>
+            <a href="#" class="book-now-button">BOOK NOW</a>
+        </div>
+
+        <div class="column">
+            <img src="<?php bloginfo('template_directory') ?>/eo.jpg" alt="Enterprise Office Image">
+            <h3>Enterprise Office</h3> 
+            <ul>
+            <li>Good for teams 10 pax and up that require a customized layout </li>
+            <li>Options to include private meeting rooms</li>
+            <li>Personalized arrival experience with your brand at the entrance wall</li>
+            <li>Access to Shared Amenities</li>
+        </ul>
+            <a href="#" class="book-now-button">BOOK NOW</a>
+        </div>
+
+        <div class="column">
+            <img src="<?php bloginfo('template_directory') ?>/hd.jpg" alt="Hot Desk Image">
+            <h3>Hot Desk</h3>
+            <ul>
+            <li>Good for individuals or small teams </li>
+            <li>Hot desks in open coworking space</li>
+            <li>AAccess to meeting rooms, event spaces, and other Shared Amenities</li>
+            <li>Optional locker storage</li>
+        </ul>
+            <a href="#" class="book-now-button">BOOK NOW</a>
+        </div>
+    </div>
+</section>
+    
+    
+<footer>
+<div class="footer-content">
+    <div class="left-footer">
+        <div class="navbar-brand">
+            <img src="<?php bloginfo('template_directory') ?>/Asset 2.svg" alt="Workviaa Logo">
+        </div>
+        <p> Workviaa is operated by propques and is part of its flexible workspaces portfolio.</p>
+        <div class="social-icons">
+            <a href="https://www.facebook.com/your-page-url" target="_blank" class="icon"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://www.linkedin.com/in/your-profile-url" target="_blank" class="icon"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://www.instagram.com/your-profile-url" target="_blank" class="icon"><i class="fab fa-instagram"></i></a>
+        </div>
+    </div>
+    <div class="center-footer">
+        <nav>
+            <ul class="footer-menu">
+                <li><a href="#Navigation">Navigation</a></li>
+                <li><a href="#Locations">Locations</a></li>
+                <li><a href="#Pricing">Pricing</a></li>
+                <li><a href="#Faqs">Faqs</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="right-center-footer">
+        <nav>
+            <ul class="footer-menu">
+                <li><a href="#Contact">Contact</a></li>
+            </ul>
+        </nav>
+        <p>+91 7392037856</p>
+        <p>buzz@workviaa.com</p>
+    </div>
+    <div class="right-footer">
+        <div class="subscribe-section">
+            <p>Subscribe</p>
+            <input type="email" class="rounded-input" placeholder="E-mail">
+            <a href="#" class="send-button">SEND</a>
+        </div>
+    </div>
+</div>
+<hr class="footer-divider"> <!-- Add a horizontal line -->
+<p class="copyright">Copyright © 2023. Workviaa</p>
+</footer>
+    
+    
+    
+</body>
+</html>
